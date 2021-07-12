037e5968fce6a9e639842a01721ac9f405d13b7b
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-07-12 07:36:00 +0200

    Add Header with Flix h1 and searchBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import { Button, Typography } from "antd";
+import { useState } from "react";
+import { Input, Typography, Layout } from "antd";
 import "./App.css";
 
+const { Search } = Input;
 const { Title } = Typography;
+const { Header } = Layout;
 
 function App() {
+  const [search, setSearch] = useState("Search for a movie , tv show");
   return (
-    <div className="App">
-      <Title type="primary">FLIX</Title>
-      <Button type="danger" size="large">
-        Flix
-      </Button>
-    </div>
+    <>
+      <Layout>
+        {" "}
+        <Header>
+          <Title
+            style={{
+              float: "left",
+              width: "33%",
+              textAlign: "center",
+              marginTop: "10px",
+            }}
+            type="danger"
+          >
+            FLIX
+          </Title>
+          <Search
+            style={{
+              float: "right",
+              width: "33%",
+              marginTop: "10px",
+            }}
+            placeholder={search || "Search for a movie , tv show"}
+            onChange={(e) => setSearch(e.target.value)}
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={(value) => alert(value)}
+          />
+        </Header>
+      </Layout>
+    </>
   );
 }
 
